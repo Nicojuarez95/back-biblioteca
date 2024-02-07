9fa5fbc3932aec04f96db0a18ba464dd91ef53d6
Nicolas Juarez <juareznico61@gmail.com>
2024-02-07 19:51:00 -0300

    back get y post

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import './config/database.js'
 import indexRouter from './routes/index.js'
 import path from 'path';
 import { __dirname } from './utils.js'
+import cors from "cors"
 
 let app = express();
 
@@ -11,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-
+app.use(cors())
 
 app.use('/', indexRouter);
 
